c16b04dc24ab03cb16b22dcf54eda82093cd6304
zhangchaohao <1120135449@qq.com>
2019-06-21 14:33:26 +0800

    第一版

--- src/main/java/my/admin/code/common/security/AuthenticationAccessDeniedHandler.java
+++ src/main/java/my/admin/code/common/security/AuthenticationAccessDeniedHandler.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+/**
+ * 没有权限的处理类
+ */
 public class AuthenticationAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
